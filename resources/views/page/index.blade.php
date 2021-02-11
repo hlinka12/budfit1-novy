9dedd1ed8d9b67fa634a49fd1e2dd6b704c1d234
Patrik Hlinka <pato.hlinka12@gmail.com>
2021-02-11 10:51:10 +0100

    dizajn-pozadie+navbar+domov

--- resources/views/page/index.blade.php
+++ resources/views/page/index.blade.php
@@ -1,5 +1,11 @@
 @extends('layouts.layout')
 
 @section('content')
-    
+    <div class="hero">
+        <h1 class="vitajte">Vitajte na BUDFIT</h1>
+    </div>  
+
+    <div class="hero">
+        <h1 class="vitajte1">Miesto kde nájdete tie najlepšie články o zdravej výžive</h1>
+    </div>
 @endsection
